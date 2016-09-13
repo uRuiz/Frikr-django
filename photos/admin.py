c43c6e93d585f450c8f3620f6476b4fd4e536ceb
Urko Ruiz <zaszur@gmail.com>
2016-09-14 00:31:48 +0200

    Creamos el endpoint del listado de usuarios y de cracion de usuario(a medias)

--- photos/admin.py
+++ photos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from photos.models import Photo
 
 
@@ -7,6 +9,7 @@ class PhotoAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner_name', 'license', 'visibility',)
     list_filter = ('license', 'visibility',)
     search_fields = ('name', 'description',)
+    readonly_fields = ('image_tag',)
 
     fieldsets = (
         ("Name and description", {
@@ -18,13 +21,13 @@ class PhotoAdmin(admin.ModelAdmin):
             'classes': ('wide',)
         }),
         ('URL', {
-            'fields': ('url',),
+            'fields': ('url', 'image_tag',),
             'classes': ('wide',)
         }),
-        ('Licence and visbility', {
+        ('Licence and visibility', {
            'fields':  ('license', 'visibility',),
             'classes': ('wide', 'collapse',)
-        }),
+        })
     )
 
     def owner_name(self, photo):
@@ -32,5 +35,7 @@ class PhotoAdmin(admin.ModelAdmin):
     owner_name.admin_order_field = 'owner'
     owner_name.short_description = 'Propietario'
 
+    def image_tag(self, photo):
+        return mark_safe("<img src={0}>".format(photo.url))
 
 admin.site.register(Photo, PhotoAdmin)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 
+from users.api import UserListAPI
 from users.views import LogoutView, LoginView
 
 urlpatterns = [
+    # Web URLs
     url(r'^login$', LoginView.as_view(), name='users_login'),
     url(r'^logout$', LogoutView.as_view(), name='users_logout'),
+
+    # API URLs
+    url(r'^api/1.0/users/', UserListAPI.as_view(), name='api_user_list'),
 ]
